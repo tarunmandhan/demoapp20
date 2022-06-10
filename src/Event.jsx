c3c272f4adbd7785f60f268d4bf5f92bd8be5e9e
tarunmandhan <mandhantarun@gmail.com>
2022-06-10 20:21:24 +0530

    uploading

---
+++ src/Event.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const Event = () => {
+  const purple = "#8e44ad";
+  // const purple = "yellow";
+  const [bg, setBg] = useState(purple);
+  const [name, setName] = useState("Click Me");
+  const bgChange = () => {
+    // console.log("clicked");
+    let newBg = "#34495e";
+    let newName = "tarun";
+    setBg(newBg);
+    setName(newName);
+  };
+
+  const bgBack = () => {
+    setBg("#8e44ad");
+    setName("ohh no 😱😡");
+  };
+
+  return (
+    <>
+      <div style={{ backgroundColor: bg }}>
+        <button onMouseEnter={bgChange} onMouseLeave={bgBack}>
+          {name}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Event;
